8399aea7000ac677815ded208b1fa5d1921caeff
Song <49385128+songsongsun@users.noreply.github.com>
2021-11-04 15:13:53 +0800

    flink流读kafka demo编写完成

--- src/main/java/com/song/StreamingJob.java
+++ src/main/java/com/song/StreamingJob.java
@@ -18,14 +18,12 @@
 
 package com.song;
 
-import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.common.functions.ReduceFunction;
-import org.apache.flink.api.java.utils.ParameterTool;
-import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
-import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.util.Collector;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+
+import java.util.Properties;
 
 /**
  * Skeleton for a Flink Streaming Job.
@@ -70,82 +68,20 @@ public class StreamingJob {
 	}*/
 
     /**
-     * 官网上的示例
+     * 连接kafka
      *
      * @param args
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        // the host and the port to connect to
-        final String hostname;
-        final int port;
-        try {
-            final ParameterTool params = ParameterTool.fromArgs(args);
-            hostname = params.has("hostname") ? params.get("hostname") : "localhost";
-            port = params.getInt("port");
-        } catch (Exception e) {
-            System.err.println(
-                    "No port specified. Please run 'SocketWindowWordCount "
-                            + "--hostname <hostname> --port <port>', where hostname (localhost by default) "
-                            + "and port is the address of the text server");
-            System.err.println(
-                    "To start a simple text server, run 'netcat -l <port>' and "
-                            + "type the input text into the command line");
-            return;
-        }
-
         // get the execution environment
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-
-        // get input data by connecting to the socket
-        DataStream<String> text = env.socketTextStream(hostname, port, "\n");
-
-        // parse the data, group it, window it, and aggregate the counts
-        DataStream<WordWithCount> windowCounts =
-                text.flatMap(
-                        new FlatMapFunction<String, WordWithCount>() {
-                            @Override
-                            public void flatMap(String value, Collector<WordWithCount> out) {
-                                for (String word : value.split("\\s")) {
-                                    out.collect(new WordWithCount(word, 1L));
-                                }
-                            }
-                        })
-                        .keyBy(value -> value.word)
-                        .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
-                        .reduce(new ReduceFunction<WordWithCount>() {
-                                    @Override
-                                    public WordWithCount reduce(WordWithCount a, WordWithCount b) {
-                                        return new WordWithCount(a.word, a.count + b.count);
-                                    }
-                                });
-
-        // print the results with a single thread, rather than in parallel
-        windowCounts.print().setParallelism(1);
-
-        env.execute("Socket Window WordCount");
-    }
-
-
-    /**
-     * Data type for words with count.
-     */
-    public static class WordWithCount {
-
-        public String word;
-        public long count;
-
-        public WordWithCount() {
-        }
-
-        public WordWithCount(String word, long count) {
-            this.word = word;
-            this.count = count;
-        }
-
-        @Override
-        public String toString() {
-            return word + " : " + count;
-        }
+        env.enableCheckpointing(5000);
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "192.168.234.128:9092");
+        properties.setProperty("group.id", "test");
+        DataStreamSource<String> flinkdemo = env.addSource(new FlinkKafkaConsumer<String>("flinkdemo", new SimpleStringSchema(), properties));
+        flinkdemo.print();
+        env.execute("Kafka Test");
     }
 }
---
+++ src/main/java/com/song/StreamingJob1.java
@@ -0,0 +1,130 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.song;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+/**
+ * Skeleton for a Flink Streaming Job.
+ *
+ * <p>For a tutorial how to write a Flink streaming application, check the
+ * tutorials and examples on the <a href="http://flink.apache.org/docs/stable/">Flink Website</a>.
+ *
+ * <p>To package your application into a JAR file for execution, run
+ * 'mvn clean package' on the command line.
+ *
+ * <p>If you change the name of the main class (with the public static void main(String[] args))
+ * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
+ */
+public class StreamingJob1 {
+
+    /*public static void main(String[] args) throws Exception {
+        // set up the streaming execution environment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        *//*
+     * Here, you can start creating your execution plan for Flink.
+     *
+     * Start with getting some data from the environment, like
+     * 	env.readTextFile(textPath);
+     *
+     * then, transform the resulting DataStream<String> using operations
+     * like
+     * 	.filter()
+     * 	.flatMap()
+     * 	.join()
+     * 	.coGroup()
+     *
+     * and many more.
+     * Have a look at the programming guide for the Java API:
+     *
+     * http://flink.apache.org/docs/latest/apis/streaming/index.html
+     *
+     *//*
+
+		// execute program
+		env.execute("Flink Streaming Java API Skeleton");
+	}*/
+
+    /**
+     * 官网上的示例
+     *
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        // the host and the port to connect to
+        final String hostname;
+        final int port;
+        try {
+            final ParameterTool params = ParameterTool.fromArgs(args);
+            hostname = params.has("hostname") ? params.get("hostname") : "localhost";
+            port = params.getInt("port");
+        } catch (Exception e) {
+            System.err.println(
+                    "No port specified. Please run 'SocketWindowWordCount "
+                            + "--hostname <hostname> --port <port>', where hostname (localhost by default) "
+                            + "and port is the address of the text server");
+            System.err.println(
+                    "To start a simple text server, run 'netcat -l <port>' and "
+                            + "type the input text into the command line");
+            return;
+        }
+
+        // get the execution environment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        // get input data by connecting to the socket
+        DataStream<String> text = env.socketTextStream(hostname, port, "\n");
+
+        // parse the data, group it, window it, and aggregate the counts
+        DataStream<WordWithCount> windowCounts =
+                text.flatMap(
+                        new FlatMapFunction<String, WordWithCount>() {
+                            @Override
+                            public void flatMap(String value, Collector<WordWithCount> out) {
+                                for (String word : value.split("\\s")) {
+                                    out.collect(new WordWithCount(word, 1L));
+                                }
+                            }
+                        })
+                        .keyBy(value -> value.word)
+                        .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
+                        .reduce(new ReduceFunction<WordWithCount>() {
+                                    @Override
+                                    public WordWithCount reduce(WordWithCount a, WordWithCount b) {
+                                        return new WordWithCount(a.word, a.count + b.count);
+                                    }
+                                });
+
+        // print the results with a single thread, rather than in parallel
+        windowCounts.print().setParallelism(1);
+
+        env.execute("Socket Window WordCount");
+    }
+
+
+
+}
